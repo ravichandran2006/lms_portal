99412a1abb472ce2699d13071bd403bc71657c1c
ravichandran2006 <ravichandranm.23aim@kongu.edu>
2026-02-02 21:46:21 +0530

    course edit field

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/title-form.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/title-form.tsx
@@ -36,6 +36,8 @@ const formSchema = z.object({
 
 export const TitleForm = ({ initialData, courseId }: TitleFormProps) => {
     const [isEditing, setIsEditing] = useState(false);
+    const toggleEdit=()=> setIsEditing((current)=> !current);
+    const router = useRouter();
 
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
@@ -45,18 +47,24 @@ export const TitleForm = ({ initialData, courseId }: TitleFormProps) => {
   const { isSubmitting, isValid } = form.formState;
 
   const onSubmit = async (values: z.infer<typeof formSchema>) => {
-    console.log(values);
+    try {
+      await axios.patch(`/api/courses/${courseId}`, values);
+      toast.success("Course updated");
+      toggleEdit();
+      router.refresh();
+    } catch {
+      toast.error("Something went wrong");
+    }
   };
 
   return (
     <div className="mt-6 border bg-slate-100 rounded-md p-4">
         <div className="font-medium flex items-center justify-between">
       Title Form
-      <Button variant="ghost">
-        {isEditing && (
+      <Button onClick={toggleEdit}variant="ghost">
+        {isEditing ? (
             <>Cancel</>
-        )}
-        {!isEditing && (
+        ) : (
             <>
             <Pencil className="h-4 w-4 mr-2" />
             Edit title
@@ -64,6 +72,39 @@ export const TitleForm = ({ initialData, courseId }: TitleFormProps) => {
         )}
       </Button>
     </div>
+    {!isEditing && (
+      <p className="text-sm mt-2">
+        {initialData.title}
+      </p>
+    )}
+    {isEditing && (
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)}
+        className="space-y-4 mt-43">
+          <FormField
+          control={form.control}
+          name="title"
+          render={({ field }) => (
+            <FormItem>
+              <FormControl>
+                <Input
+                disabled={isSubmitting}
+                placeholder="Course title"
+                {...field}
+                />
+              </FormControl>
+              <FormMessage />
+              </FormItem>
+          )}
+          />
+          <div className="flex items-center gap-x-2">
+            <Button type="submit" disabled={isSubmitting || !isValid}>
+              Save
+            </Button>
+          </div>
+        </form>
+        </Form>
+    )}
     </div>
   );
 };
